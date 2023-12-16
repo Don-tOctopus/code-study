7431a00d6e4517b925e662032976452d7b36b1f2
YuJeongNam <73466440+uzhjd@users.noreply.github.com>
2023-12-16 11:52:32 +0900

    fix: 빠진 부분 추가

---
+++ 1weeks/남유정/모음사전.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+
+    public int solution(String word) {
+        int answer = word.length();
+        String vowels = "AEIOU";
+        int[] change = new int[]{781, 156, 31, 6, 1};
+
+        for(int i = 0; i < word.length(); i++) {
+            answer += change[i] * vowels.indexOf(word.charAt(i));
+        }
+
+        return answer;
+    }
+}
